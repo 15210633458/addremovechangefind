8f77aacc166c94aa8b5409340faa443f024df706
sunyufeng <2220438231@qq.com>
2019-01-06 14:22:08 +0800

    添加成功

--- mongodb/index.js
+++ mongodb/index.js
@@ -7,9 +7,11 @@ function getmongodb(collection, fn) {
         if (err) {
             return typeof fn == 'function' && fn(err)
         }
-        var db = con.db('1610B'); //数据库
+        var db = con.db('1610Bpeople'); //数据库
+        //console.log(db)
         var coll = db.collection(collection)
-        typeof fn == 'function' && fn(null, con, fn)
+            //console.log(coll)
+        typeof fn == 'function' && fn(null, coll, con)
     })
 }
 module.exports = {
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,50 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('../mongodb')
-console.log(mongodb)
+    //console.log(mongodb)
     /* GET users listing. */
     //用户的增删改查
 
-//增
-router.get('/', function(req, res, next) {
-
+//所有数据的渲染
+router.get('/api/list', function(req, res, next) {
+    mongodb.getmongodb("people", function(err, coll, con) {
+        if (err) {
+            return res.json({ code: 0, mes: error })
+        }
+        //console.log(coll)
+        coll.find().toArray(function(error, result) {
+            if (error) {
+                res.json({ code: 0, mes: error })
+            } else {
+                // console.log(result)
+                res.json({ code: 1, data: result })
+                con.close()
+            }
+        })
+    })
 });
 
+//增
+router.post('/api/add', function(req, res, next) {
+    //console.log(req.body)
+    var parse = req.body,
+        name = parse.name,
+        age = parse.age,
+        phone = parse.phone,
+        add = parse.add,
+        Id = parse.Id;
+    mongodb.getmongodb('people', function(err, coll, con) {
+        if (err) {
+            res.json({ code: 0, mes: err })
+        } else {
+            coll.insert({ name: name, age: age, phone: phone, add: add, Id: Id }, function(error, result) {
+                if (error) {
+                    res.json({ code: 0, mes: error })
+                } else {
+                    res.json({ code: 1, mes: "添加成功" })
+                }
+            })
+        }
+    })
+});
 module.exports = router;
